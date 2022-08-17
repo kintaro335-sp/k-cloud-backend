acd8ba719eca0edcb085cdee6ca98b577b749ecd
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-08-16 20:55:09 -0500

    agregado compresion y proteccion

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(compression());
+  app.use(csurf());
 
   app.useGlobalPipes(new ValidationPipe());
 
