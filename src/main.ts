ef158718affa44fd17be6c4109108350fa36275e
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-08-16 22:59:25 -0500

    correccio de error con csurf

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(compression());
-  app.use(csurf());
-
   app.useGlobalPipes(new ValidationPipe());
-
+  app.use(cookieParser());
+  app.use(csurf({ cookie: { sameSite: true } }));
   await app.listen(5000);
 }
 bootstrap();
