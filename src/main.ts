3d5e22655e424a29cb6d0a23b639e2064c31996d
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-08-17 00:12:32 -0500

    CORS

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   app.use(csurf({ cookie: { sameSite: true } }));
+  app.enableCors({ credentials: true, origin: ['http://192.168.50.181:3000', 'http://localhost:3000'] });
   await app.listen(5000);
 }
 bootstrap();
