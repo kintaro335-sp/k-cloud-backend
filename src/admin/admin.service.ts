47f9f94be0c5478416bc3cfb1e09e5620f3e87a4
kintaro335-sp <kintaroponce.uni@gmail.com>
2022-08-30 22:24:57 -0500

    agrgeado link de descarga

--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 
 @Injectable()
-export class AdminService {}
+export class AdminService implements OnModuleInit, OnModuleDestroy {
+  constructor() {}
+
+  onModuleInit() {}
+
+  onModuleDestroy() {}
+}
