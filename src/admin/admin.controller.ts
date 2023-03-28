67e29a2227cba7f2e1a97a2adbd406fbe5bdf987
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-03-27 21:55:04 -0600

    added guard to avoid the deletion of the first user

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -14,6 +14,7 @@ import { RegisterDTO } from '../auth/dtos/Register.dto';
 // guards
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { AdminGuard } from './guards/admin.guard';
+import { FirstUserGuard } from './guards/first-user-guard';
 // decorators
 import { RequireAdmin } from './decorators/admin.decorator';
 
@@ -76,6 +77,7 @@ export class AdminController {
   }
 
   @RequireAdmin(true)
+  @UseGuards(FirstUserGuard)
   @Delete('/users/delete/:userid')
   async deleteUser(@Param('userid') userid: string): Promise<MessageResponse> {
     return this.authServ.deleteUser(userid);
