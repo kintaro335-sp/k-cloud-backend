4f94f6e681742639ad393df67f95754e504f6805
kintaro335-sp <kintaroponce.uni@gmail.com>
2023-03-28 22:19:15 -0600

    agregado opciones de ssl

---
+++ src/main-https.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+import { AppModule } from './app.module';
+import { AppClusterService } from './app-cluster.service';
+import * as compression from 'compression';
+import * as cookieParser from 'cookie-parser';
+import * as csurf from 'csurf';
+import whiteList from './cors';
+import { readFileSync } from 'fs';
+
+async function bootstrap() {
+  const httpskey = readFileSync(process.env.SSL_KEY_PATH);
+  const httpscert = readFileSync(process.env.SSL_CERT_PATH);
+  const passphrase = process.env.SSL_PASSPHRASE;
+
+  const app = await NestFactory.create(AppModule, { httpsOptions: { key: httpskey, cert: httpscert, passphrase } });
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({ credentials: true, origin: whiteList });
+  app.use(compression());
+  await app.listen(5000);
+}
+
+bootstrap();
